b4ab86a3ebf3635d65925e8b669ff3b91f45e27e
JessMarcela <jgarciaguiza@gmail.com>
2020-08-11 23:29:50 -0500

    Modificando medicamento

--- Proyecto/src/Controlador/Main.java
+++ Proyecto/src/Controlador/Main.java
@@ -1,6 +1,16 @@
 
 package Controlador;
 
+import Modelo.Proceso;
+
 public class Main {
+    
+   public static void main(String[] args) {
+        
+       Proceso proceso = new Proceso();
+       
+    }
+    
+    
     
 }
