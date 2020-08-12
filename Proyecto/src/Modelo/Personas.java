b15ca7364f4d77c348eb4beff6f255a401161fb3
JessMarcela <jgarciaguiza@gmail.com>
2020-08-11 21:25:57 -0500

    Empezando

--- Proyecto/src/Modelo/Personas.java
+++ Proyecto/src/Modelo/Personas.java
@@ -4,5 +4,26 @@ package Modelo;
 
 public class Personas {
     private String nom;
-    private String prueba2;
+    private String usuario;
+    private String contrasena;
+
+    public Personas(String nom, String usuario, String contrasena) {
+        this.nom = nom;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+    
+    
 }
---
+++ Proyecto/src/Modelo/Proceso.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import Vista.InOut;
+import java.util.ArrayList;
+
+
+public class Proceso {
+    
+    ArrayList<Personas> personas = new ArrayList();
+    
+    InOut inOut = new InOut();
+    
+    public void crearUsuario(){
+
+        String nombre = inOut.solicitarNombre("Digite su nombre: ");
+        String  usuario = inOut.solicitarNombre("Digite su nombre de usuario: ");
+        String contrasena = inOut.solicitarNombre("Digite su contraseña: ");
+           
+        Personas persona = new Personas(nombre,usuario,contrasena);
+        
+        personas.add(persona);
+    }
+    
+    public void ingresar(){
+        
+        String usuario = inOut.solicitarNombre("Digite su usuario");
+        
+        int posicion = verificarUsuario(usuario);
+        
+        if(posicion != -1){
+            
+            String contrasena = inOut.solicitarNombre("Digite su contraseña: ");
+            
+            if(verificarContrasena(contrasena, posicion)){
+                
+                
+            }
+                
+        } else
+            inOut.mostrarResultado("El usuario es incorrecto.");
+        
+        
+    }
+    
+    public int verificarUsuario(String usuario){
+        
+        for(int i=0 ; i<personas.size(); i++){
+            
+            if(personas.get(i).getUsuario().equals(usuario))
+                    return i;
+            
+        }
+        
+        return -1;
+        
+    }
+    
+     
+    
+    public boolean verificarContrasena(String contrasena, int posicion){
+        
+        if(personas.get(posicion).getContrasena().equals(contrasena))
+            return true;
+        else
+            return false;
+           
+    }
+    
+}
